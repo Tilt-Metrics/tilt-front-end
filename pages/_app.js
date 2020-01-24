cc57e791c9220bdd0fc7d42d0941ca5189fc36d6
chriswynn <chris@wynncodes.com>
2020-01-24 14:13:57 -0500

    feat: add theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "minireset.css";
+import { ThemeProvider } from "emotion-theming";
+import theme from "../lib/theme";
 
 export default function({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
