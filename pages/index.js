1e537ef0698a05e536ae017d15e87246f6d6d46d
chriswynn <chris@wynncodes.com>
2020-01-24 12:20:10 -0500

    feat: add index page data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
-export default function() {
-  return <div>Tilt</div>;
+export default function({ data }) {
+  return <div>{JSON.stringify(data)}</div>;
+}
+
+export async function unstable_getStaticProps() {
+  const fetch = require("node-fetch");
+
+  const data = await fetch(
+    "https://admin.tiltmetrics.com/wp-json/wp/v2/pages/?slug=home"
+  )
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      return data[0].acf;
+    })
+    .catch(err => console.log(err));
+
+  return {
+    props: {
+      data
+    }
+  };
 }
