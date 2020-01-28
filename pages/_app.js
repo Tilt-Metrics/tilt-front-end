4dbe6806c08f369526c4397d3a4e9657b8642c2c
chriswynn <chris@wynncodes.com>
2020-01-28 10:35:08 -0500

    feat: add fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import "minireset.css";
+import { Global, css } from "@emotion/core";
 import { ThemeProvider } from "emotion-theming";
 import theme from "../lib/theme";
 
 export default function({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Global
+        styles={css`
+          @import url("https://fonts.googleapis.com/css?family=Work+Sans:300");
+          @import url("https://rsms.me/inter/inter.css");
+          html {
+            font-family: "Inter", sans-serif;
+          }
+        `}
+      />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
