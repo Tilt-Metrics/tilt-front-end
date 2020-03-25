67dd8fa3bc2e50884e5434fd2a124b485fa1317a
chriswynn <chris@wynncodes.com>
2020-03-25 13:59:05 -0400

    refactor: adjust Testimonial styles

--- components/Home/Testimonial.js
+++ components/Home/Testimonial.js
@@ -5,92 +5,101 @@ import Wrapper from "../UI/Wrapper";
 
 function Testimonial(props) {
   return (
-    <Wrapper>
-      <div
-        css={theme => ({
-          alignItems: "center",
-          justifyContent: "center",
-          fontFamily: theme.fonts.body,
-          display: "flex",
-          height: "100%",
-          width: "100%",
-          marginBottom: theme.spacing[3]
-        })}
-      >
+    <div css={{ backgroundColor: "white" }}>
+      <Wrapper>
         <div
-          css={theme =>
-            mq({
-              backgroundColor: theme.colors.slate,
-              maxWidth: theme.maxWidths[0],
-              height: "100%",
-              width: "100%",
-              padding: ["30px", "70px 108px"]
-            })
-          }
+          css={theme => ({
+            backgroundImage: 'url("/img/texture.png")',
+            backgroundSize: "cover",
+            backgroundPosition: "bottom",
+            alignItems: "center",
+            justifyContent: "center",
+            fontFamily: theme.fonts.body,
+            display: "flex",
+            position: "relative",
+            height: "100%",
+            width: "100%",
+            marginBottom: theme.spacing[3],
+            paddingTop: theme.spacing[3]
+          })}
         >
-          <h3
-            css={theme => ({
-              fontSize: theme.headingSizes[0],
-              textAlign: "center",
-              marginBottom: 40
-            })}
-          >
-            {props.quote}
-          </h3>
           <div
-            css={{
-              display: "flex",
-              justifyContent: "center",
-              paddingBottom: 48,
-              borderBottom: "1px solid rgba(39, 40, 61, 0.1)",
-              marginBottom: 48
-            }}
+            css={theme =>
+              mq({
+                backgroundColor: theme.colors.slate,
+                maxWidth: theme.maxWidths[0],
+                height: "100%",
+                postion: "realtive",
+                width: "100%",
+                padding: ["30px", "70px 108px"],
+                zIndex: 10
+              })
+            }
           >
-            <img
-              css={{ width: 56, height: 56, marginRight: "16px" }}
-              src={props.image}
-            />
+            <h3
+              css={theme => ({
+                fontSize: theme.headingSizes[0],
+                textAlign: "center",
+                marginBottom: 40
+              })}
+            >
+              {props.quote}
+            </h3>
             <div
               css={{
                 display: "flex",
-                flexDirection: "column",
-                justifyContent: "center"
+                justifyContent: "center",
+                paddingBottom: 48,
+                borderBottom: "1px solid rgba(39, 40, 61, 0.1)",
+                marginBottom: 48
               }}
             >
-              <p css={{ fontWeight: "bold" }}>{props.author}</p>
-              <p>{props.title}</p>
+              <img
+                css={{ width: 56, height: 56, marginRight: "16px" }}
+                src={props.image}
+              />
+              <div
+                css={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center"
+                }}
+              >
+                <p css={{ fontWeight: "bold" }}>{props.author}</p>
+                <p>{props.title}</p>
+              </div>
+            </div>
+            <div
+              css={mq({
+                display: "grid",
+                gridTemplateColumns: ["repeat(1, 1fr)", "repeat(3, 1fr)"],
+                gridGap: ["15%", "5%"],
+                gridRowGap: ["30%", "auto"],
+                gridAutoRows: "min-content",
+                gridAutoFlow: "row",
+                height: "100%"
+              })}
+            >
+              {props.stats.map((s, idx) => (
+                <div key={idx} css={{ textAlign: "center" }}>
+                  <h4
+                    css={theme => ({
+                      fontFamily: theme.fonts.display,
+                      color: theme.colors.brightBlue,
+                      fontSize: theme.headingSizes[3],
+                      marginBottom: theme.spacing[0]
+                    })}
+                  >
+                    {s.headline}
+                  </h4>
+                  <p>{s.subheadline}</p>
+                </div>
+              ))}
             </div>
           </div>
-          <div
-            css={mq({
-              display: "grid",
-              gridTemplateColumns: ["repeat(1, 1fr)", "repeat(3, 1fr)"],
-              gridGap: ["15%", "5%"],
-              gridRowGap: ["30%", "auto"],
-              gridAutoRows: "min-content",
-              gridAutoFlow: "row",
-              height: "100%"
-            })}
-          >
-            {props.stats.map((s, idx) => (
-              <div key={idx} css={{ textAlign: "center" }}>
-                <h4
-                  css={theme => ({
-                    fontFamily: theme.fonts.display,
-                    color: theme.colors.brightBlue,
-                    fontSize: theme.headingSizes[3],
-                    marginBottom: theme.spacing[0]
-                  })}
-                >
-                  {s.headline}
-                </h4>
-                <p>{s.subheadline}</p>
-              </div>
-            ))}
-          </div>
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </div>
   );
 }
 
