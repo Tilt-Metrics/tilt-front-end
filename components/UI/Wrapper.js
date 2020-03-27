12be39965094b0816e743b5142642ed504475611
chriswynn <chris@wynncodes.com>
2020-03-27 00:11:16 -0400

    refactor: adjust Wrapper mobile padding

--- components/UI/Wrapper.js
+++ components/UI/Wrapper.js
@@ -1,10 +1,13 @@
 import styled from "@emotion/styled";
+import { mq } from "../../lib/utils";
 
-const Wrapper = styled.div(props => ({
-  margin: "0 auto",
-  maxWidth: props.theme.maxWidths[props.width],
-  padding: "0px 30px"
-}));
+const Wrapper = styled.div(props =>
+  mq({
+    margin: "0 auto",
+    maxWidth: props.theme.maxWidths[props.width],
+    padding: ["0px 15px", "0px 30px"]
+  })
+);
 
 Wrapper.defaultProps = {
   width: 1
