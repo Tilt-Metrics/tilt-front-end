ea0b53c68e46d8cd98af4439753393bca2139427
chriswynn <chris@wynncodes.com>
2020-04-20 11:05:24 -0400

    fix: update contact form

--- components/Home/ContactForm.js
+++ components/Home/ContactForm.js
@@ -1,28 +1,31 @@
 /** @jsx jsx */
+import { useState } from "react";
 import { jsx } from "@emotion/core";
 
-function handleSubmit(e) {
-  const formData = document.querySelector("#form");
-  e.preventDefault();
-  console.log(formData);
-  fetch(
-    "https://admin.tiltmetrics.com/wp-json/contact-form-7/v1/contact-forms/76/feedback/",
-    {
-      method: "POST",
-      body: {
-        "your-name": "Test",
-        "your-email": "chris@wynncodes.com",
-        "your-message": "Hello"
-      }
-    }
-  )
-    .then(e => {
-      console.log(e);
-    })
-    .catch(err => console.log(err));
-}
-
 function ContactForm(props) {
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = e => {
+    const form = document.querySelector("#form");
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("your-name", form.elements[0].value);
+    formData.append("your-email", form.elements[1].value);
+    formData.append("your-message", form.elements[2].value);
+
+    fetch(
+      "https://admin.tiltmetrics.com/wp-json/contact-form-7/v1/contact-forms/76/feedback",
+      {
+        method: "POST",
+        body: formData
+      }
+    )
+      .then(e => {
+        setSubmitted(true);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div
       css={theme => ({
@@ -68,58 +71,66 @@ function ContactForm(props) {
         >
           {props.subheadline}
         </p>
-        <form
-          id="form"
-          onSubmit={handleSubmit}
-          css={theme => ({
-            display: "flex",
-            flexDirection: "column",
-            marginTop: theme.spacing[1],
-            "& input, & textarea": {
-              background: "none",
-              border: "none",
-              borderBottom: "2px solid rgba(39, 40, 61, .2)",
-              fontSize: theme.copySizes[1],
-              fontFamily: theme.fonts.body,
-              marginBottom: theme.spacing[1],
-              outline: "none",
-              transition: ".3s ease",
-              "&:focus": {
-                borderBottom: `2px solid ${theme.colors.coral}`
-              }
-            }
-          })}
-        >
-          <input name="your-name" placeholder="Name" type="text" required />
-          <input
-            name="your-email"
-            placeholder="Email"
-            type="text"
-            type="email"
-            required
-          />
-          <textarea
-            name="your-message"
-            placeholder="Message"
-            required
-            css={{ height: 200 }}
-          />
-          <button
+        {!submitted && (
+          <form
+            id="form"
+            onSubmit={handleSubmit}
             css={theme => ({
-              background: "none",
-              border: `1px solid ${theme.colors.coral}`,
-              color: theme.colors.coral,
-              fontSize: theme.copySizes[1],
-              margin: "0 auto",
-              cursor: "pointer",
-              padding: "18px 20px",
-              width: "40%"
+              display: "flex",
+              flexDirection: "column",
+              marginTop: theme.spacing[1],
+              "& input, & textarea": {
+                background: "none",
+                border: "none",
+                borderBottom: "2px solid rgba(39, 40, 61, .2)",
+                fontSize: theme.copySizes[1],
+                fontFamily: theme.fonts.body,
+                marginBottom: theme.spacing[1],
+                outline: "none",
+                transition: ".3s ease",
+                "&:focus": {
+                  borderBottom: `2px solid ${theme.colors.coral}`
+                }
+              }
             })}
-            type="submit"
           >
-            {props.buttonText}
-          </button>
-        </form>
+            <input name="your-name" placeholder="Name" type="text" required />
+            <input
+              name="your-email"
+              placeholder="Email"
+              type="text"
+              type="email"
+              required
+            />
+            <textarea
+              name="your-message"
+              placeholder="Message"
+              required
+              css={{ height: 200 }}
+            />
+            <button
+              css={theme => ({
+                background: "none",
+                border: `1px solid ${theme.colors.coral}`,
+                color: theme.colors.coral,
+                fontSize: theme.copySizes[1],
+                margin: "0 auto",
+                cursor: "pointer",
+                padding: "18px 20px",
+                width: "40%"
+              })}
+              type="submit"
+            >
+              {props.buttonText}
+            </button>
+          </form>
+        )}
+        {submitted && (
+          <p css={{ marginTop: "50px" }}>
+            Your message was successfully sent. We can’t wait to read it, and
+            will be in contact with you shortly!
+          </p>
+        )}
       </div>
     </div>
   );
